38ae21dafce6b45866b4eb48b8abdef0f91bd6f1
brovatten <alexander_bridgwater@live.se>
2020-11-15 00:23:24 +0100

    Implemented the keyword algorithm and now storing all jobs in a dictionary with url as key and number of hits with relevant keywords as value.

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from bs4 import BeautifulSoup as soup
 import re
 from selenium import webdriver
 
+
 def currentpageurl(url, currentpage):
-    nextPageIndex = currentpage*10
+    nextPageIndex = currentpage * 10
     url = 'https://se.indeed.com/jobb?q=Internship&start=' + str(nextPageIndex)
     return url
+
 
 def returnHTMLpage(url):
     uClient = uReq(url)
@@ -37,74 +39,77 @@ def returnApplicationLink(links):
         app_list.append(link_found.get("href"))
     return app_list
 
-#def retrieveBtnLink(parsed_html,pagenumber):
-   # pageValue = 10*pagenumber
-   # href_str = "/jobs?q=Internship&amp;start=" + "" + str(pageValue)
+
+# def retrieveBtnLink(parsed_html,pagenumber):
+# pageValue = 10*pagenumber
+# href_str = "/jobs?q=Internship&amp;start=" + "" + str(pageValue)
 #    link_found = parsed_html.find("a", attrs={'href': re.compile("^" + "" + href_str)})
 
-def filtrateJobs_KeyWords(listOfJobs, keywords):
-    job_list_temp = []
+def filtrateJobs_KeyWords(listOfJobs, preferredKeys, requiredKeys):
+    job_dict = {}  # Storing {url:preferedkey_counter}
     for url in listOfJobs:
         parsed_html = returnHTMLpage(url)
-        jobDescription = parsed_html.find('div', {"id":"jobDescriptionText"})
-        if(goodMatch(jobDescription, keywords)):
-            job_list_temp.append(url)
+        jobDescription = parsed_html.find('div', {"id": "jobDescriptionText"})
+        numberOfpreferedkeys = keywordAlgo(jobDescription, preferredKeys, requiredKeys)
+        if (numberOfpreferedkeys > -1):
+            job_dict[url] = numberOfpreferedkeys
+    job_dict = sortDictbyPreferedKeys(job_dict)
+    return job_dict
 
+
+def sortDictbyPreferedKeys(job_dict):
+    return {k: v for k, v in sorted(job_dict.items(), key=lambda item: item[1])}
+
+
+def containsRequiredKeys(jobDescription, requiredKeys):
+    requiredWordCounter = 0
+    for word in requiredKeys:
+        if (jobDescription.findAll(text=re.compile(word))):
+            requiredWordCounter += 1
+    if (requiredWordCounter == len(requiredKeys)):
+        return True
+    else:
+        return False
+
+
+def preferedKeysCounter(jobDescription, preferredKeys):
+    preferedWordCounter = 0
+    for word in preferredKeys:
+        if (jobDescription.findAll(text=re.compile(word))):
+            preferedWordCounter += 1
+    return preferedWordCounter
 
 
 ###Slutade här när jag inte kunde hitta ord i html parsingen
-def goodMatch(jobDescription, keywords):
-    counter = 0
-    print(jobDescription)
-    print(type(jobDescription))
-    for word in keywords:
-        #print("hej")
-        print("word:" + word)
-        if(jobDescription.findAll(text=re.compile(word))):
-            counter += 1
-    if(counter> 0):
-        print(counter)
-        return True
-    print(counter)
+def keywordAlgo(jobDescription, preferredKeys, requiredKeys):
+    if (containsRequiredKeys(jobDescription, requiredKeys)):
+        return preferedKeysCounter(jobDescription, preferredKeys)
+    return -1
 
-
-
-
-
+def printFilteredJobs(job_dict):
+    for key in job_dict:
+        print(key + "  counter:" + str(job_dict[key]))
 
 ###################################################
 
-#my_url = 'https://se.indeed.com/Internship-jobb'
 my_url = 'https://se.indeed.com/jobb?q=Internship&start=0'
 currentpage = 0
-required_key_words = ['bachelors']
-#prefered_key_words = ['cations']
+required_key_words = ['are']
+preferred_key_words = ['cations']
 job_list = []
+
 while True:
-    if(currentpage == 1):
+    if (currentpage == 1):
         print(currentpage)
         break
     else:
-                 #starting at 0
-        my_url = currentpageurl(my_url,currentpage)
-        currentpage+=1
-        #print(currentpage)
-        #print(my_url)
+        # starting at 0
+        my_url = currentpageurl(my_url, currentpage)
+        currentpage += 1
         parsed_HTML = returnHTMLpage(my_url)
         jobPortalPage = returnJobPortalApplications(parsed_HTML)
         job_list = job_list + jobPortalPage
 
-
-filtrateJobs_KeyWords(job_list,key_words)
-print()
+job_dict = filtrateJobs_KeyWords(job_list, preferred_key_words, required_key_words)
+printFilteredJobs(job_dict)
 ###################################################
-
-#print(retrieveBtnLink(parsed_HTML,1))
-
-#filtratedList = filtrateJobs_KeyWords(jobPortalPage, ["We are responsible", "Thank you", "1", "Visa", "1-2","Nordea"])
-
-
-#my_url2 = 'https://se.indeed.com/viewjob?jk=bef25f175416ca9d&tk=1emib66i8st9i800&from=serp&vjs=3'
-#page = returnHTMLpage(my_url2)
-#jobDescription = page.find('div', {"id":"jobDescriptionText"})
-#print(jobDescription)
